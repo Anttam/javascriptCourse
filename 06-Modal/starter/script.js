e09b41dc66035488bd4f319bcc85c70051931be0
Anttam <96652586+Anttam@users.noreply.github.com>
2022-04-25 23:48:55 -0400

    finished the 'modal' project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// variable declaration
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.btn');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//function declaration
+
+const openModal = function () {
+  console.log('Open button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  console.log('Modal closed');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// event listner for opening/closing modal with click
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// event listener for closing modal with escape key
+
+document.addEventListener('keydown', function (e) {
+  if (e.code === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+  console.log(e.key);
+});
